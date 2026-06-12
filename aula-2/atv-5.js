ff15dfaf86843d74d39405f6ee92d95723d46616
SMariotto <mariotto.sim@gmail.com>
2026-06-12 12:13:08 -0300

    aula2

---
+++ aula-2/atv-5.js
@@ -0,0 +1,6 @@
+let number = 20 
+
+while (number >= 20) {
+    console.log (number)
+    number = number-2
+}
---
+++ aula-2/atv-6.js
@@ -0,0 +1,8 @@
+let number = 5
+let multiplicador = 1
+
+while (multiplicador <= 10) {
+
+    console.log (number*multiplicador)
+    multiplicador = multiplicador+1
+}
